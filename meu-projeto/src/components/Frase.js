1ed969129969e10116cee034ff7dc33285e52596
Luís Massuchini <luis.massuchini@gmail.com>
2022-12-18 08:07:40 -0300

    update 1 description

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -2,7 +2,7 @@ import styles from './Frase.module.css'
 function Frase() {
     return(
         <div className={styles.fraseContainer}>
-            <p className={styles.fraseContent}>Este é um componente com uma frase</p>
+            <p className={styles.fraseContent}>Este é um componente com uma Frase</p>
         </div>
     )
 }
