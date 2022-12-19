b077644ec17c6089cb26a3f4d9deb7b562163aca
Luís Massuchini <luis.massuchini@gmail.com>
2022-12-19 10:38:47 -0300

    Aula 8

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SayMyname from './components/SayMyName';
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const nome = 'Maria'
@@ -14,7 +15,12 @@ function App() {
       <SayMyname nome="Felipe" />
       <SayMyname nome="João" />
       <SayMyname nome={nome} />
-      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder/150" />
+      <Pessoa 
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder/150" />
+        <List/>
 
     </div>
   );
